f9209e90a6de796cd16a2f79482dbf4931e41a8c
y-solb <ysolb99@gmail.com>
2021-08-02 15:00:51 +0900

    feat: 1-4-alias

---
+++ 1-types/1-4-alias.ts
@@ -0,0 +1,30 @@
+{
+  /**
+   * Type Aliases : 새로운 타입을 정의할 수 있음
+   */
+  type Text = string; // Text라는 새로운 타입은 문자열을 말합니다~
+  const name1: string = 'solbi';
+  const name2: Text = 'solbi';
+  const address: Text = 'korea';
+  type Num = number;
+  type Student = {
+    name: string;
+    age: number;
+  };
+  const student: Student = {
+    name: 'ellie',
+    age: 12,
+  };
+
+  /**
+   * String Literal Types : 타입을 문자열로 지정
+   */
+  type Name = 'name';
+  let ellieName: Name;
+  ellieName = 'name';
+  type JSON = 'json';
+  const json: JSON = 'json';
+
+  type Boal = true;
+  const isCat: Boal = true;
+}
