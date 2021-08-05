5a1747961a0fcd567b43502aa422bc28ecd3ba21
y-solb <ysolb99@gmail.com>
2021-08-05 16:15:53 +0900

    feat: 2-types-projects-quiz

---
+++ 2-types-projects-quiz/calculator.ts
@@ -0,0 +1,27 @@
+/**
+ * Let's make a calculator 🧮
+ */
+
+type Command = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+function calculate(command: Command, num1: number, num2: number): number {
+  switch (command) {
+    case 'add':
+      return num1 + num2;
+    case 'substract':
+      return num1 - num2;
+    case 'multiply':
+      return num1 * num2;
+    case 'divide':
+      return num1 / num2;
+    case 'remainder':
+      return num1 % num2;
+    default:
+      throw new Error('unknown error');
+  }
+}
+
+console.log(calculate('add', 1, 3)); // 4
+console.log(calculate('substract', 3, 1)); // 2
+console.log(calculate('multiply', 4, 2)); // 8
+console.log(calculate('divide', 4, 2)); // 2
+console.log(calculate('remainder', 5, 2)); // 1
